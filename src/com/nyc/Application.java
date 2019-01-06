6071e471eb17323ea41d0b1dfe08f4fef9a37810
JOSE-PC\HICHAM <kninich5@aol.com>
2019-01-06 17:04:35 -0500

    edit Application.java

--- src/com/nyc/Application.java
+++ src/com/nyc/Application.java
@@ -11,10 +11,10 @@ public class Application {
 		System.out.print("Guess your number: ");
 		int guessedNum=scan.nextInt();
 		Random rand=new Random();
-		int winnerNum=rand.nextInt(99)+1;
+		int winnerNum=rand.nextInt(9)+1;
 		
 		if(guessedNum==winnerNum) {
-			System.out.println("WINNER!!!!!");
+			System.out.println("\n\t********WINNER********");
 			System.exit(0);
 		}
 		else
