f9752822f4bc93a7599051cc3de9c9357a5ce34a
lamya <lamya@DESKTOP-PN9SI23>
2026-03-26 23:36:27 -0400

    conflict example

--- src/com/nyc/Application.java
+++ src/com/nyc/Application.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 
 public class Application {
 
+	//conflict example
 	public static void main(String[] args) {
 		while(true) {
 		Scanner scan=new Scanner(System.in);
 		System.out.print("Guess your number from 1 to 9: ");
 		int guessedNum=scan.nextInt();
 		Random rand=new Random();
-		int winnerNum=rand.nextInt(99)+1;
+		int winnerNum=rand.nextInt(9)+1;
 		System.out.println("winner number is " + winnerNum);
 		
 		if(guessedNum==winnerNum) {
