53d17e57fa269543998a7b4026a7302d819d0f1f
lamya <lamya@DESKTOP-PN9SI23>
2026-03-26 23:28:52 -0400

    add comment

--- src/com/nyc/Application.java
+++ src/com/nyc/Application.java
@@ -6,18 +6,22 @@ import java.util.Scanner;
 public class Application {
 
 	public static void main(String[] args) {
+		//use loop
 		while(true) {
 		Scanner scan=new Scanner(System.in);
 		System.out.print("Guess your number from 1 to 9: ");
 		int guessedNum=scan.nextInt();
+		//generate guessed number
 		Random rand=new Random();
 		int winnerNum=rand.nextInt(9)+1;
 		
 		if(guessedNum==winnerNum) {
+			//print success msg
 			System.out.println("\n\t********WINNER********");
 			System.exit(0);
 		}
 		else
+			//print failure msg
 			System.out.println("LOSER!");
 		}
 	}
